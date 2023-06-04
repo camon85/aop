179a2b7d7da83ab648575da70dcfd91716c23bd8
camon <camon85@gmail.com>
2023-06-04 23:22:01 +0900

    프록시와 내부 호출 - 대안2 지연 조회 (ApplicationContext)

---
+++ src/main/java/hello/aop/internalcall/CallServiceV2.java
@@ -0,0 +1,26 @@
+package hello.aop.internalcall;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class CallServiceV2 {
+
+    private final ApplicationContext applicationContext;
+
+    public CallServiceV2(ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
+    }
+
+    public void external() {
+        log.info("call external");
+        CallServiceV2 callServiceV2 = applicationContext.getBean(CallServiceV2.class);
+        callServiceV2.internal(); // 외부 메서드 호출
+    }
+
+    public void internal() {
+        log.info("call internal");
+    }
+}
