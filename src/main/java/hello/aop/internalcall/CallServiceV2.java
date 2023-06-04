cbc6a4511493e8a9f08165e4fce0d7769bab73d2
camon <camon85@gmail.com>
2023-06-04 23:23:33 +0900

    프록시와 내부 호출 - 대안2 지연 조회 (ObjectProvider)

--- src/main/java/hello/aop/internalcall/CallServiceV2.java
+++ src/main/java/hello/aop/internalcall/CallServiceV2.java
@@ -1,22 +1,22 @@
 package hello.aop.internalcall;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.ApplicationContext;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
 public class CallServiceV2 {
 
-    private final ApplicationContext applicationContext;
+    private final ObjectProvider<CallServiceV2> callServiceV2ObjectProvider;
 
-    public CallServiceV2(ApplicationContext applicationContext) {
-        this.applicationContext = applicationContext;
+    public CallServiceV2(ObjectProvider<CallServiceV2> callServiceV2ObjectProvider) {
+        this.callServiceV2ObjectProvider = callServiceV2ObjectProvider;
     }
 
     public void external() {
         log.info("call external");
-        CallServiceV2 callServiceV2 = applicationContext.getBean(CallServiceV2.class);
+        CallServiceV2 callServiceV2 = callServiceV2ObjectProvider.getObject();
         callServiceV2.internal(); // 외부 메서드 호출
     }
 
